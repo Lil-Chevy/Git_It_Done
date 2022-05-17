6c516e36836ad2b3a89e6bcad2fc7591eebfb5a1
Cheddar Bay Biscuits <nowaczewskijack@gmail.com>
2022-05-17 18:17:53 -0400

    added code snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,14 @@
-var getUserRepos=function(user){
-    // format the github api URL
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    // make a request to the URL.
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-        console.log(data);
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
     });
-});
+  });
 };
-getUserRepos();
+
+getUserRepos("lernantino");
